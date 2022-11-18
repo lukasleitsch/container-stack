69af2508b1f5b5b18310d3a13d30ef827f9f8578
Lukas Leitsch <lukas@leitsch.org>
2022-11-18 15:50:23 +0100

    cleanup

--- hakatime/scripts/backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /docker/hakatime
-source .env
-docker-compose exec db pg_dump -c -U ${DB_USER} ${DB_NAME} |  gzip --rsyncable > ./backups/hakatime.sql.gz
--- hakatime/scripts/restore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /docker/hakatime
-source .env
-
-gunzip < backups/dump.sql.gz | docker-compose exec -T db psql -U ${DB_USER}
